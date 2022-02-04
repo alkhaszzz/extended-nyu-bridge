86adbe664d370c088a1a46b33ffef9824e320180
alkhaszzz <98994860+alkhaszzz@users.noreply.github.com>
2022-02-04 15:40:43 -0500

    Create an3744_hw4_q1.cpp

---
+++ HW4/an3744_hw4_q1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int counter;
+
+    cout << "please enter a poositive integer: ";
+    cin >> n;
+
+    counter = 1;
+    cout << "section a" << endl;
+    while (counter <= n) {
+        cout << (2 * counter) << endl;
+        counter++;
+    }
+
+
+    cout << "section b" << endl;
+    for (counter = 1; counter <= n; counter++) {
+        cout << (2 * counter) << endl;
+    }
+}
+
